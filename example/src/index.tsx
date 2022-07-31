008c280a4e1717a9c8343832037587cf0dad9695
Ben <bmsteve96@gmail.com>
2022-07-31 14:47:05 +0300

    update ripple effect (release)

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,15 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@flexcodelabs/button";
-import "@flexcodelabs/button/lib/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Button
       label="Button"
+      disabled
+      loading
       style={{
         padding: 10,
         background: "black",
+        position: "relative",
+        color: "white",
+        overflow: "hidden",
+        gap: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "200px",
+      }}
+    />
+    <Button
+      label="Button"
+      loading
+      style={{
+        padding: 10,
+        background: "black",
+        position: "relative",
+        color: "white",
+        overflow: "hidden",
+        gap: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "200px",
+      }}
+    />
+    <Button
+      label="Button"
+      style={{
+        padding: 10,
+        background: "black",
+        position: "relative",
+        color: "white",
+        overflow: "hidden",
+        gap: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "200px",
+      }}
+    />
+    <Button
+      label="Button"
+      loading
+      spinnerBgColor="green"
+      style={{
+        padding: 10,
+        background: "green",
+        position: "relative",
+        color: "white",
+        overflow: "hidden",
+        gap: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "200px",
+      }}
+    />
+    <Button
+      label="Button"
+      loading
+      spinnerBgColor="blue"
+      style={{
+        padding: 10,
+        background: "blue",
+        position: "relative",
+        color: "white",
+        overflow: "hidden",
+        gap: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "200px",
       }}
     />
   </React.StrictMode>,
